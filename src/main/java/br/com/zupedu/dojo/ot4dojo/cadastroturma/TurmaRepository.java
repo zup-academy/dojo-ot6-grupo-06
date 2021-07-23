662f37626a2000b5c4090649ad7c2b1070da0bc6
unknown <mauricio.rodriguesbd@gmail.com>
2021-07-23 14:51:38 -0300

    piloto(Mauricio Rodrigues) co-piloto(Jessica Yara)

---
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/cadastroturma/TurmaRepository.java
@@ -0,0 +1,9 @@
+package br.com.zupedu.dojo.ot4dojo.cadastroturma;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TurmaRepository extends JpaRepository<TurmaEntity,Long>{
+
+}
